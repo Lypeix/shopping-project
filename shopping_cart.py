24a78c59999d28ffa0ffb218ee8b801a1a4b430f
Lypeix <patrykkudla1010@gmail.com>
2026-06-16 16:42:42 +0200

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,14 +1,17 @@
 #SHOPPING PROJECT
 
+# DICTIONARIES
+
+products = {
+    'cola': {'name': 'Cola', 'price': 4, 'currency': 'PLN'}, 
+    'mango': {'name': 'Mango', 'price': 8, 'currency': 'PLN'},
+    'manga': {'name': 'Manga', 'price': 70, 'currency': 'PLN'},
+    'meat':{'name': 'Meat', 'price': 40, 'currency': 'PLN'},
+    'suit':{'name': 'Suit', 'price': 500, 'currency': 'PLN'}
+    }
 
 
-print(f"Products available:") 
-product_names = ['Cola', 'Mango', 'Manga', 'Meat', 'Suit']
-for idx, product in enumerate(product_names, start=1):
-    print(f"{idx}. {product}")
-
-
-
+# FUNCTIONS
 
 def user_choice(prompt, valid_choices):
 
@@ -19,21 +22,28 @@ def user_choice(prompt, valid_choices):
         else:
             print(f"Invalid choice, please choose from: {', ' .join(valid_choices)} ")
 
-product_choice = user_choice("Choose a product by typing in desired number!"
-                "\n1. Cola"
-                "\n2. Manga"
-                "\n3. Suit"
-                "\n> ",
-                ["1", "2", "3"]
-                )
 
-product_map = {
-    "1": "Cola",
-    "2": "Manga",
-    "3": "Suit"
-    }
+def main():
 
-product = product_map[product_choice]
+    prompt_lines = ['Choose product by typing corresponding number']
 
-print(f"Your cart currently consists of: {product}")
+    product_keys = list(products.keys())
 
+    for idx, key in enumerate(product_keys, start=1):
+        product = products[key]
+        prompt_lines.append(f"{idx}. {product['name']} - {product['price']} {product['currency']}") 
+    
+    prompt_lines.append(f"> ")
+    prompt =  "\n".join(prompt_lines)
+
+    valid_choices = [str(i) for i in range(1, len(product_keys) + 1)]
+
+    choice_num = user_choice(prompt, valid_choices)
+
+    product_key = product_keys[int(choice_num) - 1]
+    chosen_product = product[product_key]
+
+    print(f'You have selected {chosen_product['name']}!')
+
+if __name__ == "__main__":
+    main()
