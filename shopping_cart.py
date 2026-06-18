9ea27b62eee532f7badd41aa29f110efcb4584bd
Lypeix <patrykkudla1010@gmail.com>
2026-06-18 10:50:54 +0200

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,22 +1,6 @@
 #SHOPPING PROJECT
 
-# DICTIONARIES
-
-products = {
-    'cola': {'name': 'Cola', 'price': 4, 'currency': 'PLN', 'stock': 84}, 
-    'mango': {'name': 'Mango', 'price': 8, 'currency': 'PLN', 'stock': 12},
-    'manga': {'name': 'Manga', 'price': 70, 'currency': 'PLN', 'stock': 41},
-    'meat':{'name': 'Meat', 'price': 40, 'currency': 'PLN', 'stock': 321},
-    'suit':{'name': 'Suit', 'price': 500, 'currency': 'PLN', 'stock': 3}
-    }
-
-cart = {
-    'items': [],
-    'item_count': 0,
-    'total_price': 0
-}
-
-# FUNCTIONS
+# INITIALIZATION
 
 def user_choice(prompt, valid_choices):
     """Loops until user selects a valid choice"""
@@ -27,21 +11,73 @@ def user_choice(prompt, valid_choices):
         else:
             print(f"Invalid choice, please choose from: {', ' .join(valid_choices)} ")
 
+def initialization():
+    
+    user_name = input("Welcome! How would you like to name your account?\n> ")
+
+    user_currency = user_choice(f"Hello {user_name}! Please pick your currency: "
+                "\nPLN"
+                "\nUSD"
+                "\nEUR"
+                "\nYEN"
+                "\n> ",
+                ["pln", "usd", "eur", "yen"]
+                )
+
+    user_account = {
+    'name': user_name,
+    'currency': user_currency,
+    'currency_display': user_currency.upper()
+    }
+
+
+    return user_account
+
+# DICTIONARIES
+
+user_account = initialization()
+
+
+products = {
+    'cola': {'name': 'Cola', 'price': 4, 'currency': user_account['currency_display'], 'stock': 84}, 
+    'mango': {'name': 'Mango', 'price': 8, 'currency': user_account['currency_display'], 'stock': 12},
+    'manga': {'name': 'Manga', 'price': 70, 'currency': user_account['currency_display'], 'stock': 41},
+    'meat':{'name': 'Meat', 'price': 40, 'currency': user_account['currency_display'], 'stock': 321},
+    'suit':{'name': 'Suit', 'price': 500, 'currency': user_account['currency_display'], 'stock': 3}
+    }
+
+cart = {
+    'items': [],
+    'item_count': 0,
+    'total_price': 0
+}
+
+# CART FUNCTIONS
+
+
+def display_cart():
+    """Displays catalog"""
+    prompt_lines = ['Choose a product by typing in corresponding number: ']
+
+    product_keys = list(products.keys()) # ['cola', 'mango', 'manga', etc.]
+
+    for idx, key in enumerate(product_keys, start=1):
+        product = products[key]
+        
+        prompt_lines.append(f"{idx}. {product['name']} - {product['price']} {product['currency']}"
+                            f" - Stock: {product['stock']} units") 
+        
+    prompt_lines.append("> ")
+
+    return '\n'.join(prompt_lines), product_keys
 
 def add_to_cart():
-    """Displays catalog and lets the user choose what kind of stuff they want to be added there"""
-    while True:
-        prompt_lines = ['Choose product by typing corresponding number: ']
+    """Lets user pick products"""
 
-        product_keys = list(products.keys()) # ['cola', 'mango', 'manga', etc.]
+    while True:    
 
-        for idx, key in enumerate(product_keys, start=1):
-                product = products[key]
-                prompt_lines.append(f"{idx}. {product['name']} - {product['price']} {product['currency']} - Stock: {product['stock']} units ") 
-        prompt_lines.append("> ")
+        prompt, product_keys = display_cart()
 
-        prompt = "\n".join(prompt_lines)
-            
         valid_choices = [str(i) for i in range(1, len(product_keys) + 1)]
 
         choice_num = user_choice(prompt, valid_choices)
@@ -49,6 +85,7 @@ def add_to_cart():
         product_key = product_keys[int(choice_num) - 1] # converts choice string to integer and subtracts 1 so that guy's choice reflects index
         chosen_product = products[product_key]
         
+
         cart['items'].append(product_key)
         cart['item_count'] += 1
         cart['total_price'] += chosen_product['price']
@@ -59,7 +96,7 @@ def add_to_cart():
         print(f'-=-!CART STATUS!-=-')
         print(f"Items: {', '.join(item_names)}")
         print(f"Item count: {cart['item_count']}") 
-        print(f"Total price: {cart['total_price']}")
+        print(f"Total price: {cart['total_price']} {user_account['currency_display']}")
 
 
         answer = user_choice('Would you like to choose another item? (Yes/No) \n> ',
@@ -75,12 +112,10 @@ def add_to_cart():
             return True
 
         
-
-
 def main():
-    
-    add_to_cart()
 
+    display_cart()
+    add_to_cart()
 
 if __name__ == "__main__":
     main()
